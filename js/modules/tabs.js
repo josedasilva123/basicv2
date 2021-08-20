3f1789fc9708bc3ddc57be0dc1bc62ace848ae73
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2021-08-20 19:03:38 -0300

    Prev e Next no módulo Tabs

--- js/modules/tabs.js
+++ js/modules/tabs.js
@@ -4,26 +4,72 @@ export default class Tabs {
     this.activeClass = "ativo";
   }
 
-  selectItem(items, content, i) {
+  selectItem(menu, items, content, i) {    
     [...content, ...items].forEach((item) => {
       item.classList.remove(this.activeClass);
     });
 
     items[i].classList.add(this.activeClass);
     content[i].classList.add(this.activeClass);
+    menu.setAttribute('data-index', i);
   }
 
-  tabScript(menu) {
+  previousItem(menu, items, content){
+    const currentIndex = +menu.getAttribute('data-index');
+    let index;
+    if (currentIndex == 0){
+      index = items.length - 1;
+    } else {
+      index = currentIndex - 1;
+    }
+    console.log(index);  
+    console.log(currentIndex);
+
+    this.selectItem(menu, items, content, index);
+  }
+
+  nextItem(menu, items, content){
+    const currentIndex = +menu.getAttribute('data-index');
+    let index;
+    if (currentIndex == (items.length - 1)){
+      index = 0;
+    } else {
+      index = currentIndex + 1;
+    }  
+    this.selectItem(menu, items, content, index);
+  }
+
+  tabScript(menu) { 
     const items = menu.querySelectorAll("li");
     const content = document.querySelectorAll(
       `[data-tab-content="${menu.dataset.tabMenu}"`
     );
+    
+    const previous = document.querySelectorAll(`[data-tab-prev="${menu.dataset.tabMenu}"`);
+    const next = document.querySelectorAll(`[data-tab-next="${menu.dataset.tabMenu}"`);
+
+    if(previous){
+      previous.forEach(element => {
+        element.addEventListener("click", () => {
+          this.previousItem(menu, items, content);
+        })
+      })      
+    }
+
+    if(next){
+      next.forEach(element => {
+        element.addEventListener("click", () => {
+          this.nextItem(menu, items, content);
+        })
+      })
+    }
 
     items.forEach((item, index) => {
       item.addEventListener("click", () => {
-        this.selectItem(content, items, index);
+        this.selectItem(menu, items, content, index);
       });
     });
+    menu.setAttribute('data-index', 0);
   }
 
   autoPlay(menu) {
